f36eb11a869bd5d5796e1f4538130948c6cb0cb9
Marin <marin.knezevic1et2@gmail.com>
2021-05-20 20:33:44 +0200

    home page controller

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,6 +4,16 @@ class Home//controller for the home page and index method
 {
     function index()
     {
-        echo "home page index method";
+        $this->view("home");//calling function below
+    }
+
+    function view($view)//reads from the view directory and displays page for user
+    {
+        if(file_exists("../app/views/". $view .".php"))
+        {
+            include "../app/views/". $view .".php";
+        }else{
+            include "../app/views/404.php";
+        }
     }
 }
