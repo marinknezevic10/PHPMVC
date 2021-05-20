61ebe12764ae1f7b991f1c0168cc005f5e75035d
Marin <marin.knezevic1et2@gmail.com>
2021-05-20 21:06:55 +0200

    image cropping class and instantiating a model

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,6 +4,9 @@ class Home extends Controller//controller for the home page and index method
 {
     function index()
     {
+        $image_class = $this->loadModel("image_class");
+        //checking if its correct
+        //show($image_class);
         $this->view("home");//calling function below
     }
 
