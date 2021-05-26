fd6624edc16cd68801d3ac78b637c7eef4534b7d
Marin <marin.knezevic1et2@gmail.com>
2021-05-26 21:33:08 +0200

    image cropping class

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -18,6 +18,17 @@ class Home extends Controller//controller for the home page and index method
 
         $data['posts'] = $result;
 
+        //image cropping
+        $image_class = $this->loadModel("image_class");
+
+        if(is_array($data['posts']))
+        {
+            foreach ($data['posts'] as $key => $value) {
+                # code...
+                $data['posts'][$key]->image = $image_class->get_thumbnail($data['posts'][$key]->image);
+            }
+        }
+
         //show($data[0]->image);//showing particular image
 
         $this->view("templates/index", $data);//calling function view
