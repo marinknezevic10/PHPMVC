0c41d75d6be8ea9b6272180cbe59d5a636ce4c69
Marin <marin.knezevic1et2@gmail.com>
2021-05-26 21:24:22 +0200

    displaying images

--- app/views/templates/index.php
+++ app/views/templates/index.php
@@ -12,18 +12,26 @@
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.</p>
             </div>  
           </header>
-          <div class="background-white full-width"> 
-            <div class="s-12 m-6 l-five">
-              <a class="image-with-hover-overlay image-hover-zoom" href="/" title="Portfolio Image">
-                <div class="image-hover-overlay background-primary"> 
-                  <div class="image-hover-overlay-content text-center padding-2x">
-                    <h3 class="text-white text-size-20 margin-bottom-10">Reference Title</h3>
-                    <p class="text-white text-size-14 margin-bottom-20">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.</p>  
+
+          <?php if(is_array($data['posts'])): ?>
+
+          <?php foreach($data['posts'] as $row): ?>
+
+            <div class="background-white full-width"> 
+              <div class="s-12 m-6 l-five">
+                <a class="image-with-hover-overlay image-hover-zoom" href="/" title="Portfolio Image">
+                  <div class="image-hover-overlay background-primary"> 
+                    <div class="image-hover-overlay-content text-center padding-2x">
+                      <h3 class="text-white text-size-20 margin-bottom-10"><?= $row->title?></h3>
+                      <p class="text-white text-size-14 margin-bottom-20"><?= $row->description?></p>  
+                    </div> 
                   </div> 
-                </div> 
-                <img class="full-img" src="<?=ASSETS?>templates/img/portfolio/thumb-01.jpg" alt=""/>
-              </a>	
-            </div>
+                  <img class="full-img" src="<?=ROOT . $row->image ?>"" alt=""/>
+                </a>	
+              </div>
+
+            <?php endforeach; ?>
+            <?php endif; ?>
            
           </div>  
         </article>
